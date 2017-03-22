fcfa25aa438efcb13ef15d0ecd7cfe35ec5f1c67
Dakota <bazookaharwell@gmail.com>
2017-03-22 09:46:04 -1000

    Item class

--- zombies.js
+++ zombies.js
@@ -8,6 +8,16 @@
  * @property {string} name
  */
 
+ class Item {
+  constructor(name) {
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+ }
+
 
 /**
  * Class => Weapon(name, damage)
